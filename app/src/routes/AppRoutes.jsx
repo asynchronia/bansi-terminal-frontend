128ec1c517a8a3e9eb742f8e05a63178083b6b95
HuzaifaBanegar <banegarhuzzi1010@gmail.com>
2024-03-02 20:08:40 +0530

    routes format

--- app/src/routes/AppRoutes.jsx
+++ app/src/routes/AppRoutes.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home'
-import Products from '../pages/Products'
+import Products from '../pages/Products/Products'
 
 const AppRoutes = () => {
   return (
