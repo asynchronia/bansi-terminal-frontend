ea1bd110e1b9661e514564512c632a3ab748ad69
HuzaifaBanegar <banegarhuzzi1010@gmail.com>
2024-03-03 14:25:57 +0530

    file structure

---
+++ app/src/pages/Products/AllProducts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AllProducts = () => {
+  return (
+    <div>
+        All Products
+    </div>
+  )
+}
+
+export default AllProducts
--- app/src/routes/AppRoutes.jsx
+++ app/src/routes/AppRoutes.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from '../pages/Home'
-import Products from '../pages/Products/Products'
+import Home from '../pages/Home/Home'
+import CreateProducts from '../pages/Products/CreateProducts'
 
 const AppRoutes = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home/>}></Route>
-        <Route path='/products' element={<Products/>}></Route>
+        <Route path='/products' element={<CreateProducts/>}></Route>
       </Routes>
     </div>
   )
