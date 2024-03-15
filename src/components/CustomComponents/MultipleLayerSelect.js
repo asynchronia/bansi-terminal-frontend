2c025a52368945cd1935826ac9ee0ef4d4c6480b
HuzaifaBanegar <banegarhuzzi1010@gmail.com>
2024-03-15 17:54:22 +0530

    categories dropdown list changed

---
+++ src/components/CustomComponents/MultipleLayerSelect.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+
+
+
+const MultipleLayerSelect = ({categories,setCategoryData}) => {
+ 
+
+
+  const handleCategoryClick = (categoryId, categoryName) => {
+    setCategoryData({ id: categoryId, name: categoryName, show:false });
+  };
+
+  const handleChildClick = (event, childId, childName) => {
+    event.stopPropagation();
+    setCategoryData({ id: childId, name: childName , show:false });
+  };
+
+  const handleGrandchildClick = (event, grandchildId, grandchildName) => {
+    event.stopPropagation();
+    setCategoryData({ id: grandchildId, name: grandchildName, show:false  });
+  };
+
+  return (
+    <div style={{ maxHeight: "200px", overflowY: "scroll" }}>
+      {categories.map((category) => (
+        <div
+          style={{
+            border: "1px solid #dee2e6",
+            padding: "2px 5px",
+            cursor: "pointer",
+          }}
+          key={category._id}
+          className="option"
+          onClick={() => handleCategoryClick(category._id, category.name)}
+        >
+          {`➡ ${category.name}`}
+          {category.children && (
+            <div className="child-options">
+              {category.children.map((child) => (
+                <div
+                  style={{
+                    border: "1px solid #dee2e6",
+                    padding: "2px 5px",
+                    cursor: "pointer",
+                  }}
+                  key={child._id}
+                  className="child-option"
+                  onClick={(event) =>
+                    handleChildClick(event, child._id, child.name)
+                  }
+                >
+                  → {child.name}
+                  {child.children && (
+                    <div
+                      style={{
+                        border: "1px solid #dee2e6",
+                        padding: "2px 5px",
+                        cursor: "pointer",
+                      }}
+                      className="grandchild-options"
+                    >
+                      {child.children.map((grandchild) => (
+                        <div
+                          key={grandchild._id}
+                          className="grandchild-option"
+                          onClick={(event) =>
+                            handleGrandchildClick(
+                              event,
+                              grandchild._id,
+                              grandchild.name
+                            )
+                          }
+                        >
+                          → {grandchild.name}
+                        </div>
+                      ))}
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default MultipleLayerSelect;
--- src/pages/Items/CreateItems.js
+++ src/pages/Items/CreateItems.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react"
+import React, { useEffect, useRef, useState } from "react";
 import {
   Row,
   Col,
@@ -8,124 +8,132 @@ import {
   Input,
   CardTitle,
   Form,
-} from "reactstrap"
-import "react-toastify/dist/ReactToastify.css"
-import Dropzone from "react-dropzone"
-import { connect } from "react-redux"
-import { setBreadcrumbItems } from "../../store/actions"
-import { Link } from "react-router-dom"
-import InputWithChips from "../../components/CustomComponents/InputWithChips"
-import { v4 as uuidv4 } from "uuid"
-import * as Yup from "yup"
-import { useFormik } from "formik"
-import AllVariantRows from "../../components/CustomComponents/AllVariantRows"
-import axios from "axios"
-import { createItemReq } from "../../service/itemService"
-import { ToastContainer, toast } from "react-toastify"
-import Standard from "../../components/CustomComponents/Standard"
+} from "reactstrap";
+import "react-toastify/dist/ReactToastify.css";
+import Dropzone from "react-dropzone";
+import { connect } from "react-redux";
+import { setBreadcrumbItems } from "../../store/actions";
+import { Link } from "react-router-dom";
+import InputWithChips from "../../components/CustomComponents/InputWithChips";
+import { v4 as uuidv4 } from "uuid";
+import * as Yup from "yup";
+import { useFormik } from "formik";
+import AllVariantRows from "../../components/CustomComponents/AllVariantRows";
+import axios from "axios";
+import { createItemReq } from "../../service/itemService";
+import { ToastContainer, toast } from "react-toastify";
+import Standard from "../../components/CustomComponents/Standard";
+import YourComponent from "../../components/CustomComponents/MultipleLayerSelect";
+import MultipleLayerSelect from "../../components/CustomComponents/MultipleLayerSelect";
 
-const CreateItems = props => {
-  const [selectedFiles, setselectedFiles] = useState([])
-  const [allCategories, setAllCategories] = useState([])
-  const [allTaxes, setAllTaxes] = useState([])
-  const [taxArr, setTaxArr] = useState([])
-  const [variantOptions, setVariantOptions] = useState([{ id: uuidv4() }])
-  const [fieldInvalid, setFieldInvalid] = useState(false)
+const CreateItems = (props) => {
+  const [selectedFiles, setselectedFiles] = useState([]);
+  const [allCategories, setAllCategories] = useState([]);
+  const [allTaxes, setAllTaxes] = useState([]);
+  const [taxArr, setTaxArr] = useState([]);
+  const [variantOptions, setVariantOptions] = useState([{ id: uuidv4() }]);
+  const [fieldInvalid, setFieldInvalid] = useState(false);
+  const [categoryData, setCategoryData] = useState({
+    name: "",
+    id: null,
+    show: false,
+  });
+
   const [variantData, setVariantData] = useState([
     { id: uuidv4(), attributes: [] },
-  ])
+  ]);
 
   const [otherData, setOtherData] = useState({
     sku: "",
     inventory: "",
     costPrice: "",
     sellingPrice: "",
-  })
+  });
 
   const notify = (type, message) => {
     if (type === "Error") {
       toast.error(message, {
         position: "top-center",
         theme: "colored",
-      })
+      });
     } else {
       toast.success(message, {
         position: "top-center",
         theme: "colored",
-      })
+      });
     }
-  }
+  };
 
-  const handleTaxes = e => {
-    const value = e.target.value
-    setTaxArr([...taxArr, value])
-  }
+  const handleTaxes = (e) => {
+    const value = e.target.value;
+    setTaxArr([...taxArr, value]);
+  };
 
   const searchCategories = async () => {
     try {
       const response = await axios.get(
-        "http://localhost:3000/api/categories/list",
-      )
-      let data = await response.data
-      setAllCategories(data?.payload?.categories)
+        "http://localhost:3000/api/categories/list"
+      );
+      let data = await response.data;
+      setAllCategories(data?.payload?.categories);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   const searchAllTaxes = async () => {
     try {
-      const response = await axios.get("http://localhost:3000/api/taxes/list")
-      let data = await response.data
-      setAllTaxes(data?.payload?.taxes)
+      const response = await axios.get("http://localhost:3000/api/taxes/list");
+      let data = await response.data;
+      setAllTaxes(data?.payload?.taxes);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
-    searchCategories()
-    searchAllTaxes()
-  }, [])
+    searchCategories();
+    searchAllTaxes();
+  }, []);
 
   const handleVariantChange = (id, name, value) => {
-    const updatedVariants = variantData.map(variant => {
+    const updatedVariants = variantData.map((variant) => {
       if (variant.id === id) {
         if (name === "attributes") {
           return {
             ...variant,
             attributes: [...variant.attributes, value],
-          }
+          };
         }
         return {
           ...variant,
           [name]: value,
-        }
+        };
       }
-      return variant
-    })
-    setVariantData(updatedVariants)
-  }
+      return variant;
+    });
+    setVariantData(updatedVariants);
+  };
 
   const handleOtherChange = (name, value) => {
     setOtherData({
       ...otherData,
       [name]: value,
-    })
-  }
+    });
+  };
 
-  const handleItemCreation = async values => {
+  const handleItemCreation = async (values) => {
     try {
-      const response = await createItemReq(values)
+      const response = await createItemReq(values);
       if (response.success === true) {
-        notify("Success", response.message)
+        notify("Success", response.message);
       } else {
-        notify("Error", response.message)
+        notify("Error", response.message);
       }
     } catch (error) {
-      notify("Error", error.message)
+      notify("Error", error.message);
     }
-  }
+  };
 
   const validation = useFormik({
     enableReinitialize: true,
@@ -146,25 +154,35 @@ const CreateItems = props => {
     validationSchema: Yup.object({
       title: Yup.string().required("Please Enter Item Name"),
       hsnCode: Yup.string().required("Please Enter HSN Code"),
-      category: Yup.string().required("Please Select Category"),
+      category: Yup.string()
+        .required("Please Select Category")
+        .min(3, "Please Select Category"),
       itemType: Yup.string().required("Please Select Item Type"),
     }),
-    onSubmit: values => {
-      let unhandled = false
+    onSubmit: (values) => {
+      let unhandled = false;
       if (values.itemType === "variable") {
         for (let i = 0; i < variantData.length; i++) {
           if (
             variantData[i].attributes.length === 0 ||
             variantData[i].inventory === ""
           ) {
-            unhandled = true
+            unhandled = true;
           } else if (
             variantData[i].sku === "" ||
             variantData[i].costPrice === "" ||
             variantData[i].sellingPrice === ""
           ) {
-            unhandled = true
+            unhandled = true;
           }
+        }
+      } else if (values.itemType === "service") {
+        if (
+          otherData.sku === "" ||
+          otherData.costPrice === "" ||
+          otherData.sellingPrice === ""
+        ) {
+          unhandled = true;
         }
       } else {
         if (
@@ -173,98 +191,103 @@ const CreateItems = props => {
           otherData.sellingPrice === "" ||
           otherData.inventory === ""
         ) {
-          unhandled = true
+          unhandled = true;
         }
       }
 
       if (unhandled) {
-        setFieldInvalid(false)
-        notify("Error", "Please enter all the fields")
-        return
+        setFieldInvalid(false);
+        notify("Error", "Please enter all the fields");
+        return;
       } else {
         if (values.itemType === "variable") {
-          const finalVariant = variantData.map(element => {
-            const { id, ...rest } = element
-            return rest
-          })
-          values.variants = [...finalVariant]
-        } else {
-          values.variants = [{ ...otherData }]
+          const finalVariant = variantData.map((element) => {
+            const { id, ...rest } = element;
+            return rest;
+          });
+          values.variants = [...finalVariant];
+        } else if(values.itemType === "standard"){
+          values.variants = [{ ...otherData }];
+        }else{
+          const{ inventory, ...rest}= otherData;
+          values.variants=[{...rest}];
         }
 
-        values.images = [...selectedFiles]
-        values.taxes = [...taxArr]
+        values.images = [...selectedFiles];
+        values.taxes = [...taxArr];
+        values.category = categoryData.id;
+        // console.log(values);
         handleItemCreation(values)
       }
     },
-  })
+  });
   const handleAddRow = () => {
-    const newRow = { id: uuidv4() }
-    setVariantOptions([...variantOptions, newRow])
-  }
+    const newRow = { id: uuidv4() };
+    setVariantOptions([...variantOptions, newRow]);
+  };
 
   const handleAddVariantData = () => {
-    const newRow = { id: uuidv4(), attributes: [] }
-    setVariantData([...variantData, newRow])
-  }
+    const newRow = { id: uuidv4(), attributes: [] };
+    setVariantData([...variantData, newRow]);
+  };
 
-  const handleDeleteRow = id => {
-    const deletedOption = variantOptions.find(option => option.id === id)
-    const updatedRows = variantOptions.filter(row => row.id !== id)
-    setVariantOptions(updatedRows)
+  const handleDeleteRow = (id) => {
+    const deletedOption = variantOptions.find((option) => option.id === id);
+    const updatedRows = variantOptions.filter((row) => row.id !== id);
+    setVariantOptions(updatedRows);
 
     if (deletedOption) {
-      const deletedAttributeName = deletedOption.name
-      const updatedVariantData = variantData.map(el => {
+      const deletedAttributeName = deletedOption.name;
+      const updatedVariantData = variantData.map((el) => {
         if (el.attributes) {
           el.attributes = el.attributes.filter(
-            attr => !attr.hasOwnProperty(deletedAttributeName),
-          )
+            (attr) => !attr.hasOwnProperty(deletedAttributeName)
+          );
         }
-        return el
-      })
+        return el;
+      });
 
-      setVariantData(updatedVariantData)
+      setVariantData(updatedVariantData);
     }
-  }
+  };
 
-  const handleDeleteVariantData = id => {
-    const updatedRows = variantData.filter(row => row.id !== id)
-    setVariantData(updatedRows)
-  }
+  const handleDeleteVariantData = (id) => {
+    const updatedRows = variantData.filter((row) => row.id !== id);
+    setVariantData(updatedRows);
+  };
 
   //Handles BreadCrumbs
   const breadcrumbItems = [
     { title: "Lexa", link: "#" },
     { title: "Create Items", link: "/create-items" },
-  ]
+  ];
 
   useEffect(() => {
-    props.setBreadcrumbItems("CreateItems", breadcrumbItems)
-  })
+    props.setBreadcrumbItems("CreateItems", breadcrumbItems);
+  });
 
   //Handle File Upload
   function handleAcceptedFiles(files) {
-    const updatedFiles = files.map(file =>
+    const updatedFiles = files.map((file) =>
       Object.assign(file, {
         preview: URL.createObjectURL(file),
         formattedSize: formatBytes(file.size),
-      }),
-    )
-    setselectedFiles([...selectedFiles, updatedFiles[0]])
+      })
+    );
+    setselectedFiles([...selectedFiles, updatedFiles[0]]);
   }
 
   /**
    * Formats the size
    */
   function formatBytes(bytes, decimals = 2) {
-    if (bytes === 0) return "0 Bytes"
-    const k = 1024
-    const dm = decimals < 0 ? 0 : decimals
-    const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
+    if (bytes === 0) return "0 Bytes";
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
 
-    const i = Math.floor(Math.log(bytes) / Math.log(k))
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i]
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
   }
 
   return (
@@ -272,10 +295,10 @@ const CreateItems = props => {
       <ToastContainer position="top-center" theme="colored" />
       <Form
         className="form-horizontal mt-4"
-        onSubmit={e => {
-          e.preventDefault()
-          validation.handleSubmit()
-          return false
+        onSubmit={(e) => {
+          e.preventDefault();
+          validation.handleSubmit();
+          return false;
         }}
       >
         <Row>
@@ -325,26 +348,32 @@ const CreateItems = props => {
                       </p>
                     ) : null}
                   </div>
+
                   <div className="mt-3 mb-0">
-                    <label className="col-form-label">Choose Category</label>
-                    <select
-                      onChange={validation.handleChange}
-                      id="category"
+                    <label className="form-label">Select Category</label>
+                    <label
                       name="category"
-                      value={validation.values.category}
-                      onBlur={validation.handleBlur}
+                      id="category"
+                      onClick={() => {
+                        setCategoryData({
+                          ...categoryData,
+                          show: !categoryData.show,
+                        });
+                      }}
                       className="form-control"
                     >
-                      <option>Category</option>
-                      {allCategories.map(e => (
-                        <option value={e._id}>{e.name}</option>
-                      ))}
-                    </select>
-                    {validation.errors.category ? (
-                      <p style={{ color: "red" }}>
-                        {validation.errors.category}
-                      </p>
+                      {categoryData.name !== ""
+                        ? `Category: ${categoryData.name}`
+                        : `Select Category`}
+                    </label>
+                    {categoryData.show ? (
+                      <MultipleLayerSelect
+                        categories={allCategories}
+                        setCategoryData={setCategoryData}
+                      />
                     ) : null}
+
+                   
                   </div>
                   <div className="mt-3">
                     <Label>Item Short Description</Label>
@@ -423,7 +452,7 @@ const CreateItems = props => {
                         className="form-control"
                       >
                         <option>Select Tax</option>
-                        {allTaxes.map(e => (
+                        {allTaxes.map((e) => (
                           <option value={e._id}>{e.name}</option>
                         ))}
                       </select>
@@ -461,8 +490,8 @@ const CreateItems = props => {
                   style={{ display: "flex", justifyContent: "space-evenly" }}
                 >
                   <Dropzone
-                    onDrop={acceptedFiles => {
-                      handleAcceptedFiles(acceptedFiles)
+                    onDrop={(acceptedFiles) => {
+                      handleAcceptedFiles(acceptedFiles);
                     }}
                   >
                     {({ getRootProps, getInputProps }) => (
@@ -512,7 +541,7 @@ const CreateItems = props => {
                             </Row>
                           </div>
                         </Card>
-                      )
+                      );
                     })}
                   </div>
                 </Form>
@@ -552,7 +581,7 @@ const CreateItems = props => {
           <Card>
             {validation.values.itemType == "variable" ? (
               <CardBody>
-                {variantOptions.map(row => (
+                {variantOptions.map((row) => (
                   <AttributesRow
                     key={row.id}
                     _id={row.id}
@@ -574,7 +603,7 @@ const CreateItems = props => {
                     <i className=" mdi mdi-18px mdi-plus"></i>
                   </button>
                 </div>
-                {variantData.map(row => (
+                {variantData.map((row) => (
                   <AllVariantRows
                     key={row.id}
                     _id={row.id}
@@ -597,20 +626,23 @@ const CreateItems = props => {
               </CardBody>
             ) : (
               <CardBody>
-                <Standard type={validation.values.itemType} onChange={handleOtherChange} />
+                <Standard
+                  type={validation.values.itemType}
+                  onChange={handleOtherChange}
+                />
               </CardBody>
             )}
           </Card>
         </Row>
       </Form>
     </div>
-  )
-}
+  );
+};
 
-const AttributesRow = props => {
-  const nameRef = useRef("")
-  const [inputValue, setInputValue] = useState("")
-  const [chips, setChips] = useState([])
+const AttributesRow = (props) => {
+  const nameRef = useRef("");
+  const [inputValue, setInputValue] = useState("");
+  const [chips, setChips] = useState([]);
 
   const {
     _id,
@@ -619,11 +651,11 @@ const AttributesRow = props => {
     setVariantOptions,
     variantData,
     setVariantData,
-  } = props
+  } = props;
 
   useEffect(() => {
-    setVariantOptions(prevOptions =>
-      prevOptions.map(e => {
+    setVariantOptions((prevOptions) =>
+      prevOptions.map((e) => {
         if (e.id === _id) {
           return {
             ...e,
@@ -632,36 +664,36 @@ const AttributesRow = props => {
                 ? nameRef.current
                 : null,
             chips: [...chips],
-          }
+          };
         }
-        return e
-      }),
-    )
-  }, [chips, nameRef])
+        return e;
+      })
+    );
+  }, [chips, nameRef]);
 
-  const handleInputChange = event => {
-    setInputValue(event.target.value)
-  }
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
 
-  const setNameRef = event => {
-    nameRef.current = event.target.value
-  }
+  const setNameRef = (event) => {
+    nameRef.current = event.target.value;
+  };
 
   //Handling Chips Creation on space or enter
-  const handleInputKeyPress = event => {
+  const handleInputKeyPress = (event) => {
     if (
       (event.key === "Enter" || event.key === " ") &&
       inputValue.trim() !== ""
     ) {
-      event.preventDefault()
-      setChips([...chips, inputValue.trim()])
-      setInputValue("")
+      event.preventDefault();
+      setChips([...chips, inputValue.trim()]);
+      setInputValue("");
     }
-  }
+  };
 
-  const handleChipDelete = chipIndex => {
-    setChips(chips.filter((_, index) => index !== chipIndex))
-  }
+  const handleChipDelete = (chipIndex) => {
+    setChips(chips.filter((_, index) => index !== chipIndex));
+  };
 
   return (
     <div>
@@ -699,7 +731,7 @@ const AttributesRow = props => {
         </Col>
       </Row>
     </div>
-  )
-}
+  );
+};
 
-export default connect(null, { setBreadcrumbItems })(CreateItems)
+export default connect(null, { setBreadcrumbItems })(CreateItems);
