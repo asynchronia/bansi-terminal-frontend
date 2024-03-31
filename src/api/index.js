3165e5e5eaef9e91a873cd03544d1995be0a3c0b
rahulgupta2704 <64494253+rahulgupta2704@users.noreply.github.com>
2024-03-31 18:34:16 +0530

    github pages frontend deployment changes

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = process.env.API_BASE_URL || "http://localhost:3000";
+const baseUrl = "http://bansi-terminal.us-east-1.elasticbeanstalk.com"
 
 const API_URL = {
     createItem: "/api/items/create",
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from "./store"
 
@@ -10,9 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </>
   </Provider>
 );
