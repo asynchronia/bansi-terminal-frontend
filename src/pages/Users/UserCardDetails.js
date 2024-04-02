777458629b9346c9ddb96cd341b8982abbf371bd
Aditya Armal <mr.adityaarmal@gmail.com>
2024-04-02 16:03:38 +0530

    Added RoleName, PrimaryWarehouse, Branch Access, User Role

--- src/pages/Users/UserCardDetails.js
+++ src/pages/Users/UserCardDetails.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useCallback } from 'react';
 import { Card, CardBody, Row, Col, Button } from 'reactstrap';
 import Chip from '@mui/material/Chip';
+import { getUserRoleListReq } from '../../service/usersService';
 
 
 const UserCardDetails = (user) => {
 
+    const [userRole, setUserRole] = useState('');
+
     console.log("user"+JSON.stringify(user));
     const color = user?.usersData?.status==="active" ? '#2ecc71' : "red";
+    
+    const roleData = useCallback(async (body) => {
+      const response = await getUserRoleListReq(user.usersData.role);
+      if (response && response.payload) {
+          // console.log(user.usersData.role);
+          // console.log(response?.payload.roles.filter(role=>role._id===user.usersData.role)[0].title);
+          setUserRole(response?.payload.roles.filter(role=>role._id===user.usersData.role)[0].title);
+      } 
+    });  
+
+    roleData();
+
+
 
     return (
-    <Card>
+    <Card style={ user?.usersData?.associatedBranch?.isPrimary==='true' || user?.associatedBranch?.isPrimary==='True'  ? { border: '2px solid blue' } : {border: 'none'}}>
       <CardBody>
         <Row className="align-items-center">
           <Col xs="auto">
@@ -30,7 +47,9 @@ const UserCardDetails = (user) => {
         <Row className="mt-3">
           <Col>
             <div className="d-flex flex-wrap">
-              <Chip label="Chip 1" className="mr-2"/>
+              {user?.usersData?.associatedBranch?.name ?
+               <Chip label={user?.usersData?.associatedBranch?.name} className="mr-2"/>
+                : null }
             </div>
           </Col>
         </Row>
@@ -39,7 +58,7 @@ const UserCardDetails = (user) => {
             <p>Role</p>
         </Col>
         <Col xs="6">
-            <p>{user?.usersData?.role}</p>
+            <p>{userRole}</p>
         </Col>
         <Col xs="6">
             <p>Contact</p>
@@ -49,6 +68,8 @@ const UserCardDetails = (user) => {
         </Col> 
         </Row>
       </CardBody>
+      { user?.usersData?.associatedBranch?.isPrimary==='true' || user?.associatedBranch?.isPrimary==='True' ? <div style={{fontSize:'0.9em', backgroundColor: '#e5f3f7', width:'8.5rem', borderRadius: '0.2rem', margin: '0 17rem', marginLeft: '33.07rem', textAlign: 'center'}}>Primary Warehouse</div> : <div></div> }  
+        
     </Card>
   );
 };
