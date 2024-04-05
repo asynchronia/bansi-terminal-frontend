ee1cbcead933fa6e05501371c6415fdbc86ee8e7
tayyaba <cnewtayyaba@gmail.com>
2024-04-05 15:20:15 +0530

    left hadn side menu highlight and active

---
+++ src/utility/Constants.js
@@ -0,0 +1,14 @@
+export const LINK_TO_DASHBOARD = "/dashboard";
+export const LINK_TO_ALL_ITEMS = "/items";
+export const LINK_TO_CREATE_ITEM="/create-item";
+export const LINK_TO_CATEGORIES="/categories";
+export const LINK_TO_CLIENTS="/clients";
+
+<li>
+                  <Link to="/create-order">Create Order</Link>
+                </li>
+                <li>
+                  <Link to="/orders">All Orders</Link>
+                </li>
+                <li>
+                  <Link to="/quotations">Quotations</Link>
