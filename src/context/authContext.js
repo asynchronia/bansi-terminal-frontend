4cacbe4fb22f6bb76d5ac15e2bffd13b45e90183
rahulgupta2704 <64494253+rahulgupta2704@users.noreply.github.com>
2024-04-09 03:25:04 +0530

    add component level permissions

---
+++ src/context/authContext.js
@@ -0,0 +1,19 @@
+import { createContext, useState } from "react";
+
+const AuthContext = createContext({});
+
+export const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState(() => {
+    const localAuth = localStorage.getItem('user');
+    return localAuth ? JSON.parse(localAuth) : {};
+  });
+
+  console.log('AuthProvider', auth);
+  return (
+    <AuthContext.Provider value={{ auth, setAuth }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthContext;
