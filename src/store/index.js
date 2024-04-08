9e5916b9fa854a2f76860b102fd54dfa74ca5437
tayyaba <cnewtayyaba@gmail.com>
2024-04-09 03:37:54 +0530

    dark theme using saga

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,16 @@
 import { createStore, applyMiddleware, compose } from "redux"
-//import createSagaMiddleware from "redux-saga"
+import createSagaMiddleware from "redux-saga";
 
 import rootReducer from "./reducers"
-//import rootSaga from "./sagas"
+import rootSaga from "./sagas"
 
-//const sagaMiddleware = createSagaMiddleware()
+const sagaMiddleware = createSagaMiddleware()
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
   rootReducer,
-  composeEnhancers(/* applyMiddleware(sagaMiddleware) */)
+  composeEnhancers(applyMiddleware(sagaMiddleware))
 )
-//sagaMiddleware.run(rootSaga)
+sagaMiddleware.run(rootSaga)
 
 export default store
