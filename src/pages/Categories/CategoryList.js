268667da5a89fdc5e296d782c93b41cceae72972
HuzaifaBanegar <banegarhuzzi1010@gmail.com>
2024-05-16 20:43:53 +0530

    Row height fix

--- src/pages/Categories/CategoryList.js
+++ src/pages/Categories/CategoryList.js
@@ -1,51 +1,64 @@
 import React from "react";
 import "./styles/CategoryList.scss";
 
-const CategoryList = (props) =>{
-    
-    const openCaret = (evt) =>{
-        
-        let domEle = evt.currentTarget;
-        domEle.parentElement.querySelector(".nested").classList.toggle("active");
-         domEle.classList.toggle("caret-down");
-    }
-    const getListView = (it) =>{
-      return ( 
+const CategoryList = (props) => {
+  const openCaret = (evt) => {
+    let domEle = evt.currentTarget;
+    domEle.parentElement.querySelector(".nested").classList.toggle("active");
+    domEle.classList.toggle("caret-down");
+  };
+  const getListView = (it) => {
+    return (
       <ul class="nested">
-       {
-            it?.children && it.children.length > 0 &&
-            it.children.map((itr, idx) =>{
-                
-               return(     
-               <> { itr?.children && itr.children.length > 0 ? 
-                <li>
-                   <span class="caret" onClick={openCaret}><span className="category-block">{itr.name}</span><span>{itr.description}</span></span>
-                   {getListView(itr)}
-              </li>  : <li><span className="category-block">{itr.name}</span><span>{itr.description}</span></li>}
-                
-                </>)
-               
-            })
-        }
-        </ul>
-        )
-    }
-    return(
-        <>
-            <ul id="myUL">
-                { props?.rowData &&  props.rowData.map((itr,idx) => {
-                    
-                    return ( itr?.children && itr.children.length > 0 ? 
-                        <li>
-                            <span class="caret" onClick={openCaret}><span className="category-block">{itr.name}</span><span>{itr.description}</span></span>
-                            {getListView(itr)}
-                    </li>  : <li><span className="category-block">{itr.name}</span><span>{itr.description}</span></li>
-                        )
-                    
-                    })}
-                </ul>
-        </>
-    )
-}
+        {it?.children &&
+          it.children.length > 0 &&
+          it.children.map((itr, idx) => {
+            return (
+              <>
+                {" "}
+                {itr?.children && itr.children.length > 0 ? (
+                  <li>
+                    <span class="caret" onClick={openCaret}>
+                      <span className="category-block">{itr.name}</span>
+                      <span>{itr.description}</span>
+                    </span>
+                    {getListView(itr)}
+                  </li>
+                ) : (
+                  <li>
+                    <span className="category-block">{itr.name}</span>
+                    <span>{itr.description}</span>
+                  </li>
+                )}
+              </>
+            );
+          })}
+      </ul>
+    );
+  };
+  return (
+    <>
+      <ul id="myUL">
+        {props?.rowData &&
+          props.rowData.map((itr, idx) => {
+            return itr?.children && itr.children.length > 0 ? (
+              <li>
+                <span class="caret" onClick={openCaret}>
+                  <span className="category-block">{itr.name}</span>
+                  <span>{itr.description}</span>
+                </span>
+                {getListView(itr)}
+              </li>
+            ) : (
+              <li>
+                <span className="category-block">{itr.name}</span>
+                <span>{itr.description}</span>
+              </li>
+            );
+          })}
+      </ul>
+    </>
+  );
+};
 
 export default CategoryList;
