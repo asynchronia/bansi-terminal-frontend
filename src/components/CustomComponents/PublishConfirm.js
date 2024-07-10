49466e9a711fb5605aefcacae14192295467dce7
Manas Sahoo <manas.sahoo9173.com>
2024-07-11 00:01:49 +0530

    CSS Issue Fixed

--- src/components/CustomComponents/PublishConfirm.js
+++ src/components/CustomComponents/PublishConfirm.js
@@ -1,23 +1,23 @@
 import React from "react";
 import { Card, CardBody } from "reactstrap";
 
-const PublishConfirm = ({ setPublishModal,setStatus }) => {
+const PublishConfirm = ({ setPublishModal, setStatus }) => {
   return (
     <Card>
       <CardBody>
-        <h3 className="text-center mt-3">Are You sure you Want to Publish?</h3>
-        <div style={{ display: "flex", justifyContent: "space-around" }}>
+        <h4 className="text-center my-4">Are You sure you Want to Publish?</h4>
+        <div className="d-flex justify-content-center gap-2">
           <button
             onClick={() => {
               setPublishModal(false);
               setStatus("published");
             }}
-            className="btn btn-secondary mx-3 mt-3 w-lg"
+            className="btn btn-secondary w-md"
           >
             Yes
           </button>
           <button
-          className="btn btn-primary mx-3 mt-3 w-lg"
+            className="btn btn-primary w-md"
             onClick={() => {
               setPublishModal(false);
               setStatus("draft");
